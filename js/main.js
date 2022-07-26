45e27298c0029a2f1e600ec09436baeb3d89ce02
Quoc Huy <contact.huyquoc@gmail.com>
2022-07-26 09:24:57 +0700

    add PostApi

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log("hello word");
+import postApi from './api/postApi';
+
+// console.log('hello word');
+async function main() {
+  const queryParam = {
+    _page: 1,
+    _limit: 5,
+  };
+  const response = await postApi.getAll(queryParam);
+  console.log(response);
+}
+
+main();
